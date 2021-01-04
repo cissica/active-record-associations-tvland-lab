0bcce0aeb6ca99e9d739658a0431a3b5b93f3e13
cissica <cecemartinez@hotmail.com>
2021-01-04 00:15:34 -0800

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
+require "pry"
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list
+        self.actors.collect do |actor_obj_arr|
+             first = actor_obj_arr.first_name 
+             last = actor_obj_arr.last_name
+             "#{first} #{last}"
+        end 
+    end 
 end
